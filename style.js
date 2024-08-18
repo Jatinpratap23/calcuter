84d8f41aa050febdebafaa2e7d1ac870752bbc41
Jatinpratap23 <vibupratap1@gmail.com>
2024-08-17 23:56:08 -1100

    calculater

---
+++ style.js
@@ -0,0 +1,31 @@
+const result=document.getElementsByClassName('result');
+
+function display(input) {
+    result.value=result.value+input;
+}
+function clearscreen()  {
+    result.value="";
+}
+function calculate(){
+let x=result.value;
+let number = x.split(/[\+\-\*\/]/).map(Number);//split by operators
+let operator = x.split(/[0-9]/).filter(op=>op!=="");
+
+let total=number[0];
+for(let i=0;i<operator.length;i++){
+    let operator = operators[i];
+    let nextNumber = numbers[i + 1];
+   
+        if (operator === "+") {
+            total += nextNumber;
+        } else if (operator === "-") {
+            total -= nextNumber;
+        } else if (operator === "*") {
+            else if (operator === "/") {
+                total /= nextNumber;
+            }
+    
+    }
+    }
+    result.value=total;
+}
